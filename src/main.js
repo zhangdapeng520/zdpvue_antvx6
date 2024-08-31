4a121da6a8f64845963f0895981dc58890cc9d34
张大鹏 <1156956636@qq.com>
2024-08-31 20:49:53 +0800

    新增：整合antd

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import 'ant-design-vue/dist/reset.css';
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
